08905fd9ffdaf84b95525a37ba5a2c24060fe4b6
Ryoung27 <RRYoung89@gmail.com>
2019-02-27 16:14:48 -0600

    accruate timing

--- dates.py
+++ dates.py
@@ -1,19 +1,38 @@
 import datetime
 
-d = datetime.date(2019, 2, 27)
-print(d)
+# d = datetime.date(2019, 2, 27)
+# print(d)
 
-# yesterday = datetime.date.yesterday()
-# print(yesterday)
+# # yesterday = datetime.date.yesterday()
+# # print(yesterday)
 
-today = datetime.date.today()
+# today = datetime.date.today()
+# # print(today)
+
+# # print(today.year)
+
+# # print(today.weekday())
+# # print(today.isoweekday())
+
+# tdelta = datetime.timedelta(days=7)
+
+# print(today + tdelta)
+
+# print(today - datetime.timedelta(days=10))
+
+# bday = datetime.date(2019, 12, 6)
+
+# till_birthday = bday - today
+
+# print(till_birthday)
+
+# today = datetime.time(9, 20, 10, 10000)
 # print(today)
 
-# print(today.year)
+dt_today = datetime.datetime.today()
+dt_now = datetime.datetime.now()
+dt_utcnow = datetime.datetime.utcnow()
 
-# print(today.weekday())
-# print(today.isoweekday())
-
-tdelta = datetime.timedelta(days=7)
-
-print(today + tdelta)
+print(dt_today)
+print(dt_now)
+print(dt_utcnow)
